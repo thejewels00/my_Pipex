246f1cc0d0dab3ab66bcc36256f26fc13b51a4c2
root <root@DESKTOP-EV2I5OD.localdomain>
2022-07-11 21:08:02 +0100

    pipex with leaks and norm errors

--- pipex.c
+++ pipex.c
@@ -17,39 +17,25 @@ int main(int argc, char **argv, char **env)
     (void)env;
     if (argc == 5)
     {
-        //system("pipex");
-        // ********* preparation des outils *****
-        vars.cmd1 = ft_split(argv[2], ' ');//leaks
-		vars.cmd2 = ft_split(argv[3], ' ');//leaks
+        
+        vars.cmd1 = ft_split(argv[2], ' ');
+		vars.cmd2 = ft_split(argv[3], ' ');
         env_path(&vars, env);
         vars.path = ft_substr(vars.path, 5, ft_strlen(vars.path) - 5);
         vars.env_p = ft_split(vars.path, ':');
-
-        // printf("my split path\n");// remove
-        // int i = 0;//rm
-		// while (vars.env_p[i]) //  remove 
-		// {
-		// 	printf("%s\n", vars.env_p[i]); //rm
-		// 	i++;//rm
-		// } //rm
-        //************************************************
-        //printf("Pipex\n"); // rm
 		if (pipe(vars.pip_fds) == -1)
 			err("PIPE");
         vars.pid = fork();
         if (vars.pid == 0)
         {
-            vars.infile = open(argv[1], O_RDONLY);// open file
+            close(vars.pip_fds[0]);
+            vars.infile = open(argv[1], O_RDONLY);
             if (vars.infile == -1)
 			    err("infile");
-            //printf("cmd1\n");
-            //printf("hna sedi Read ghire f child \n");
-            //with errno :D RFM
             if (dup2(vars.infile, 0) == -1 || dup2(vars.pip_fds[1], 1) == -1)
 				err("Dup2");
             close(vars.infile);
 			close(vars.pip_fds[1]);
-            close(vars.pip_fds[0]);//sedi read
             vars.cmd = ft_strjoin("/", vars.cmd1[0]);
 			while (j == -1 && vars.env_p[i])
 			{
@@ -57,25 +43,20 @@ int main(int argc, char **argv, char **env)
 				j = access(vars.jn, X_OK);
 				i++;
 			}
-            //vars.cmd1[0] = ft_strdup(vars.jn);
 			if (execve(vars.jn, vars.cmd1, env) == -1)  
 				err("execve");
         }
         else 
         {
-            wait(NULL);
-            //printf("reviens au parents 1\n");
-            close (vars.pip_fds[1]);// close write
-            //printf("hna sedi l infile \n");
             vars.pid_2 = fork();
             if (vars.pid_2 == 0)
             {
-                //printf("cmd2\n");
+                close (vars.pip_fds[1]);
                 vars.outfile = open(argv[4], O_CREAT | O_RDWR | O_TRUNC,  0644);
                 if (vars.outfile == -1)
 			        err("outfile");
                 if (dup2(vars.outfile, 1) == -1 || dup2(vars.pip_fds[0], 0) == -1)
-				    err("Dup2"); // Dup 2 error
+				    err("Dup2");
 			    close(vars.pip_fds[0]);
 			    close(vars.outfile);
                 vars.cmd = ft_strjoin("/", vars.cmd2[0]);
@@ -85,21 +66,18 @@ int main(int argc, char **argv, char **env)
 				    j = access(vars.jn, X_OK);
 				    i++;
 			    }
-			    //fprintf(stderr,"commande path :%s \n",vars.jn); // rm
-                //vars.cmd2[0] = ft_strdup(vars.jn);
 			    if (execve(vars.jn, vars.cmd2, env) == -1)  
 				    err("execve");
-                //chid 2
+
             }
             else
             {
+                close(vars.pip_fds[0]); 
+                close(vars.pip_fds[1]);
                 wait(NULL);
-                //printf("reviens au parents 2\n");
-                close(vars.pip_fds[0]); //close read
-                //printf("hna sedi l outfile\n");
+                wait(NULL);
             }
         }
-             //printf("what about me ?\n");
     }
     else 
         write(2, "usage : infile \"cmd1 -options\" \"cmd2 - option\" outfile\n", 56);
